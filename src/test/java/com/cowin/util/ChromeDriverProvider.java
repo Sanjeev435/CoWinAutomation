bba557a9a16459b042e08f8af3635921671a9887
Sanjeev <mrcool435@gmail.com>
2021-05-31 22:20:45 +0530

    Added Chrome Options

--- src/test/java/com/cowin/util/ChromeDriverProvider.java
+++ src/test/java/com/cowin/util/ChromeDriverProvider.java
@@ -1,9 +1,12 @@
 package com.cowin.util;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.HashMap;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Singleton Class for ChromeDriverProvider
@@ -20,10 +23,31 @@ public class ChromeDriverProvider {
 
 	public static WebDriver getInstance() {
 		if (driver == null) {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + driverLocation);
-			driver = new ChromeDriver();
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + driverLocation);			
+			driver = new ChromeDriver(ChromeDriverProvider.getChromeOptions());
 		}
 		return driver;
 	}
+	
+	public static ChromeOptions getChromeOptions() {
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("disable-infobars");
+        chromeOptions.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        chromeOptions.setExperimentalOption("useAutomationExtension", false);
+
+        chromeOptions.addArguments("--disable-gpu");
+        chromeOptions.addArguments("--disable-extensions");
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--window-size=1920,1200");
+
+        final HashMap<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+        chromeOptions.setExperimentalOption("prefs", prefs);
+
+        return chromeOptions;
+    }
 
 }
